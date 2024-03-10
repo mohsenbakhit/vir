179a799bb8dc2eb79e2201e8d614950c3b398461
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-03-09 19:18:26 -0800

    ADD: Ignore command inputs

--- src/vir.c
+++ src/vir.c
@@ -21,8 +21,14 @@ void EnableRawMode() {
 
     atexit(DisableRawMode);
     struct termios raw = terminal_settn;
-
-    terminal_settn.c_lflag &= ~(ECHO | ICANON | ISIG);
+    terminal_settn.c_iflag = ~(IGNBRK | BRKINT | PARMRK | ISTRIP
+                           | INLCR | IGNCR | ICRNL | IXON);
+    terminal_settn.c_oflag &= ~OPOST;
+    terminal_settn.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    terminal_settn.c_cflag &= ~(CSIZE | PARENB);
+    terminal_settn.c_cflag |= (CS8);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -30,7 +36,6 @@ int main() {
     EnableRawMode();
     char input;
     while (read(STDIN_FILENO, &input, 1) == 1 && input != 'q') {
-
     }
     return 0;
 }
