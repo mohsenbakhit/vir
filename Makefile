576aca387a4e44473214738ad7ddca3cb19a42a4
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-03-18 21:28:41 -0700

    ADD error handling for system call failures

---
+++ Makefile
@@ -0,0 +1,4 @@
+vir: src/vir.c
+	cc src/vir.c -o vir
+clean:
+	rm vir vir.o
--- src/vir.c
+++ src/vir.c
@@ -19,13 +19,15 @@ void shutdown(const char* c) {
 }
 
 void DisableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &terminal_settn);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &terminal_settn) == -1) {
+        shutdown("tcsetattr");
+    }
 }
 
 void EnableRawMode() {
-    tcgetattr(STDIN_FILENO, &terminal_settn);
-
+   if (tcgetattr(STDIN_FILENO, &terminal_settn) == -1) shutdown("tcgetattr");
     atexit(DisableRawMode);
+
     struct termios raw = terminal_settn;
     terminal_settn.c_iflag = ~(IGNBRK | BRKINT | PARMRK | ISTRIP
                            | INLCR | IGNCR | ICRNL | IXON);
@@ -35,15 +37,23 @@ void EnableRawMode() {
     terminal_settn.c_cflag |= (CS8);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) shutdown("tcsetattr");
 }
 
 // init
 
 int main() {
     EnableRawMode();
-    char input;
-    while (read(STDIN_FILENO, &input, 1) == 1 && input != 'q') {
+
+    while (1) {
+        char input = '\0';
+        if (read(STDIN_FILENO, &input, 1) == -1) shutdown("read");
+        if (iscntrl(input)) {
+            printf("%d\r\n", input);
+        } else {
+            printf("%d ('%c')\r\n", input, input);
+        }
+        if (input == 'q') break;
     }
     return 0;
 }
