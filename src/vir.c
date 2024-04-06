169e80e9c9713148c3f9617f466f434d0ebb1462
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-04-05 20:47:32 -0700

    ADD: newline, screen clears upon entering insert mode

--- src/vir.c
+++ src/vir.c
@@ -5,20 +5,33 @@
 #include <ncurses.h>
 
 int mode = 0; //0 is normal, 1 is insert
+bool is_first_char = true;
+void handle_unctrl(int ch) {
+  if (ch == 10) {
+    addch('\n');
+  }
+}
 int main(){
   initscr();
   noecho();
   cbreak();
-
+  addstr("~         VIR: VI Regressed          ~");
   while(true) {
-    char input = getch();
-
-    if (input == 'i') {
+    int input = getch();
+    if (input == 'i' && mode == 0) {
       mode = 1;
+      if (is_first_char) {
+        clear();
+        is_first_char = false;
+      }
     } else if (input == 27 && mode == 1) {
       mode = 0;
     } else if (mode == 1) {
-      addch(input);
+      if (unctrl(input)[0] == input)
+        addch(input);
+      else{
+        handle_unctrl(input);
+      }
     }
   }
 
