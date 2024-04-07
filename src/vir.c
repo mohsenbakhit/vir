cc3b1deeed64fc1740ebd92a64f71e5a3e37adc1
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-04-07 08:31:25 -0700

    ADD support for backspace

--- src/vir.c
+++ src/vir.c
@@ -4,18 +4,35 @@
 #include <termios.h>
 #include <ncurses.h>
 
-int mode = 0; //0 is normal, 1 is insert
+#define BACKSPACE_KEY 127
+#define ENTER_KEY 10
+int mode = 0; // 0 is normal, 1 is insert
 bool is_first_char = true;
+int y, x;
+
 void handle_unctrl(int ch) {
-  if (ch == 10) {
+  if (ch == ENTER_KEY) {
     addch('\n');
   }
+  if (ch == BACKSPACE_KEY) {
+    getyx(stdscr, y, x);
+    if (x > 0) {
+      move(y, x - 1);
+      addch(' ');
+    }
+  }
 }
+
 int main(){
   initscr();
   noecho();
   cbreak();
+  keypad(stdscr, TRUE);
+  
+  getyx(stdscr, y, x);
+
   addstr("~         VIR: VI Regressed          ~");
+
   while(true) {
     int input = getch();
     if (input == 'i' && mode == 0) {
@@ -27,8 +44,9 @@ int main(){
     } else if (input == 27 && mode == 1) {
       mode = 0;
     } else if (mode == 1) {
-      if (unctrl(input)[0] == input)
+      if (unctrl(input)[0] == input) {
         addch(input);
+      }
       else{
         handle_unctrl(input);
       }
