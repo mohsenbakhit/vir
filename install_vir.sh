c539ba6b84bc3c9fe3dd4509e7cbf1d5be0b878b
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-04-23 15:50:44 -0700

    FIX bash script to add symbolic link to vir

--- install_vir.sh
+++ install_vir.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Add the alias to your .bashrc file
-echo "alias vir='./vir'" >> ~/.zshrc
 
-# Source the updated .bashrc to make the alias available immediately
-source ~/.zshrc
+sudo ln -s your/full/path/to/vir /usr/local/bin/vir 
 
-echo "Alias 'vir' created for './vir '. You can now run it directly."
+echo "Alias 'vir' created for './vir '. Restart the terminal use run 'vir <filename>'"
 
